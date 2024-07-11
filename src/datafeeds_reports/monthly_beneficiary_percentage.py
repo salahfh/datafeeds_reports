af8f5265ae51b898e085724c40eb1b502c615472
Salah Eddine Saoud <salaheddine.saoud@financialhorizons.ca>
2024-07-11 14:56:26 -0500

    Reworked the report to leave only those that good for cleaning.

--- src/datafeeds_reports/monthly_beneficiary_percentage.py
+++ src/datafeeds_reports/monthly_beneficiary_percentage.py
@@ -13,15 +13,12 @@ BENEFFTYPE_MIN_COUNT = {
 }
 
 
-def read_and_filter_data(filepath: Path) -> pd.DataFrame:
-    df_raw = pd.read_csv(filepath)
+def read_and_filter_data(df_raw: pd.DataFrame) -> pd.DataFrame:
     fltr = df_raw['beneftype'].isin(BENEFFTYPE_MIN_COUNT.keys())
     df = df_raw[fltr].groupby(['Registration_Number', 'beneftype'])['beneftype'].count()
     df = df.unstack()
     df = df.fillna(0)
     df = df.astype(int)
-    df_percentage = df_raw[['percentage_sum', 'Registration_Number']].drop_duplicates().set_index('Registration_Number')
-    df = df.join(df_percentage)
     return df
 
 
@@ -34,30 +31,32 @@ def flag_errors_in_data(df: pd.DataFrame) -> pd.DataFrame:
         fltr = df[col] >= col_max_count
         df.loc[fltr, 'Error'] = True 
         df.loc[fltr]['Audit'].apply(lambda error: error.append(f'{col} more than or equal to {col_max_count}'))
-    
-    # if perecentage more than 100
-    fltr = df['percentage_sum'] > 100
-    df.loc[fltr, 'Error'] = True 
-    df.loc[fltr]['Audit'].apply(lambda error: error.append('Percentage more than 100%'))
+    return df
+
+
+def merge_into_orignal_data(df: pd.DataFrame, df_raw: pd.DataFrame) -> pd.DataFrame:
+    df_raw = df_raw.set_index('Registration_Number')
+    df = df_raw.join(df[['Error', 'Audit']], lsuffix='left', on='Registration_Number')
     return df
 
 
 def formating_output(df: pd.DataFrame) -> pd.DataFrame:
-    df['Audit'] = df['Audit'].apply(lambda errors: ';'.join(errors) if errors else '')
-
-    # filter only those with errors
-    df = df[df['Error'] == True]
-    return df
+    # filter only those without errors
+    df = df[df['Error'] != True]
+    header = [col for col in df.columns.to_list() if col not in ['Audit', 'Error']]
+    return df[header]
 
 
 def run_report(file_search_path: Path, file_pattern: str, final_output_filename: Path) -> None:
     file = discover_file(search_path=file_search_path, file_pattern=file_pattern)
-    df = read_and_filter_data(filepath=file)
+    df_raw = pd.read_csv(file)
+    df = read_and_filter_data(df_raw=df_raw)
     df = flag_errors_in_data(df=df)
+    df = merge_into_orignal_data(df=df, df_raw=df_raw)
     df = formating_output(df=df)
     finished_writing = write_output_to_csv(df=df, filename=final_output_filename)
     if finished_writing:
-        clean_processed_files(filepath=file, rename=True, remove=False)
+        clean_processed_files(filepath=file, rename=False, remove=False)
 
 
 def main(): 
